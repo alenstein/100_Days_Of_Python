5c1a362daffe8b680d3efc8766e4a945c982f79b
Allen Sibanda <sibandallen@gmail.com>
2023-08-02 00:17:58 +0200

    Day 1 for the 100_Days_Of_Code_Challenge

---
+++ day_1/band-name-generator.py
@@ -0,0 +1,12 @@
+""" Day 1 Project Band Name Generator code."""
+
+#1. Create a greeting for the user.
+print("Hello, welcome to the band name generator program.")
+#2. Ask the user for the city they grew up in.
+city = input("Which city did you grow up in? \n")
+#3. Ask the user for the name of their pet.
+pet = input("What's the name of your pet? \n")
+#4. Combine the name of their city and pet and show their band name.
+print("Your band name could be " + city + " " + pet)
+#5. make sure the input cursor moves to a new line.
+
