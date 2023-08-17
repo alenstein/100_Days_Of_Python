3047f92faba2dd9ff9cb24365d3ea9854e73a1a8
Allen Sibanda <sibandallen@gmail.com>
2023-08-17 08:09:23 +0200

    implemented version 1 of the coffee machine project

---
+++ day_15/coffee_machine.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# menu items for the coffee machine and resources in the coffee machine data.
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
