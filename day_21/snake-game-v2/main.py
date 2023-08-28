fea04daeecd502f4c12f16deee3f089db66c43ca
Allen Sibanda <sibandallen@gmail.com>
2023-08-28 12:45:39 +0200

    completed upgraded version of snake game

---
+++ day_21/snake-game-v2/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# part 1 for snake game project
+
+from gamedata import game_is_on, setup
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = setup()
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.shift_position()
+        score.update_score()
+        snake.extend()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 \
+            or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
+screen.exitonclick()
--- day_21/snake-game-v2/snake.py
+++ day_21/snake-game-v2/snake.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 
 class Snake:
@@ -14,18 +16,23 @@ class Snake:
             nx = self.segments[seg - 1].xcor()
             ny = self.segments[seg - 1].ycor()
             self.segments[seg].goto(nx, ny)
-
         self.head.penup()
-        self.head.forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_snake_segment = Turtle(shape="square")
+        new_snake_segment.color("white")
+        new_snake_segment.penup()
+        new_snake_segment.goto(position)
+        self.segments.append(new_snake_segment)
+
 
     def create_snake(self):
-        for t_index in range(3):
-            new_snake_segment = Turtle(shape="square")
-            new_snake_segment.color("white")
-            new_snake_segment.penup()
-            new_snake_segment.goto(t_index * -20, t_index)
-            self.segments.append(new_snake_segment)
-            new_snake_segment.pendown()
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
     def up(self):
         if self.head.heading() != 270:
